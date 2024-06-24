242fabd54b753893ed358e982c303d09f5266446
Jabson743 <138734239+Jabson743@users.noreply.github.com>
2024-06-23 14:29:43 -1200

    first commit

---
+++ PatternA.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PatternA{
+       public static void main(String[] args) {
+  
+       for (int numbers = 1; numbers < 7; numbers++) {
+
+            for (int number =  1; number <= numbers; number++) {
+            System.out.print(number + " ");
+    }
+            System.out.println();
+}
+         
+  }
+}
